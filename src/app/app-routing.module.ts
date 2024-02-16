29fd70304cebc456fbda3989fc7a9987123087c3
safa <safa.saoudi@esprit.tn>
2024-02-16 14:04:00 +0100

    s3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './main/main.component';
+import { ResidencesComponent } from './residences/residences.component';
+import { AddResidenceComponent } from './add-residence/add-residence.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DetailsResidenceComponent } from './details-residence/details-residence.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:"home", pathMatch:"full"},
+  {path:'home', component: HomeComponent},
+  {path:'residences', component:ResidencesComponent},
+  {path:'addResidence', component:AddResidenceComponent},
+  {path: 'residence/:x', component:DetailsResidenceComponent},
+  {path:'**', component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormsModule } from '@angular/forms';
+import { ResidencesComponent } from './residences/residences.component';
+import { AddResidenceComponent } from './add-residence/add-residence.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DetailsResidenceComponent } from './details-residence/details-residence.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    ResidencesComponent,
+    AddResidenceComponent,
+    NotFoundComponent,
+    DetailsResidenceComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
